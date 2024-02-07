3cb8d29a6c94d4858743b2932834d0e592e5b0e6
itsnot-aduck <hoangduc1662002@gmail.com>
2024-02-07 23:06:45 +0700

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,6 @@ export default async function Home() {
   const numPosts = 10
   const numProjects = 6
   const numBookmarks = 5
-  const numTools = 6
   const numBlogPosts = 4
 
   const pinnedPosts = await getPosts({
